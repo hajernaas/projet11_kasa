8a758a217a045792509a1c2c452fcb32f41adeaa
hajernaas <hnaas2702@gmail.com>
2024-02-19 13:54:14 +0100

    _error.scss

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -2,13 +2,15 @@ import { Link } from "react-router-dom";
 
 function Error() {
 	return (
-		<section className="error">
-			<h2 className="error__title">404</h2>
-			<p className="error__text">Oups! La page que vous demandez n'existe pas.</p>
-			<Link className="error__link" to="/">
-				Retourner sur la page d’accueil
-			</Link>
-		</section>
+		<main>
+			<section className="error">
+				<h1 className="error__title">404</h1>
+				<p className="error__text">Oups! La page que vous demandez n'existe pas.</p>
+				<Link className="error__link" to="/">
+					Retourner sur la page d’accueil
+				</Link>
+			</section>
+		</main>
 	);
 }
 
