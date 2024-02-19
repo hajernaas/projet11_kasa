6f166520d705d4a1f9398b8a5aefa23a9ea9d51d
hajernaas <hnaas2702@gmail.com>
2024-02-19 11:54:50 +0100

    composant error

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,5 +1,15 @@
+import { Link } from "react-router-dom";
+
 function Error() {
-	return <div>Page error 📟</div>;
+	return (
+		<section className="error">
+			<h2 className="error__title">404</h2>
+			<p className="error__text">Oups! La page que vous demandez n'existe pas.</p>
+			<Link className="error__link" to="/">
+				Retourner sur la page d’accueil
+			</Link>
+		</section>
+	);
 }
 
 export default Error;
