5bfbf5cd74da7ba09a965a6c70daaec167133b25
hajernaas <hnaas2702@gmail.com>
2024-02-26 14:48:06 +0100

    page accommodation _ détails profil  & accommodation.scss

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
-import starRed from "../assets/star-active.svg";
-import StarGray from "../assets/star-inactive.svg";
+import starRed from "../assets/start_active.png";
+import StarGray from "../assets/star_inactive.png";
 
 function Rating({ rating }) {
 	const ratingValueMax = 5;
--- src/pages/Accommodation.jsx
+++ src/pages/Accommodation.jsx
@@ -1,17 +1,49 @@
 import React from "react";
 import Data from "../data/Data.json";
-import { useParams } from "react-router-dom";
+import { useParams, Navigate } from "react-router-dom";
 import Slideshow from "../components/Slideshow";
 import Collapse from "../components/Collapse";
+import Rating from "../components/Rating";
 
 function Accommodation() {
 	const { id } = useParams();
 	const selectedAppartment = Data.find((appart) => appart.id === id);
 
+	if (selectedAppartment === undefined) {
+		return <Navigate to="/error" />;
+	}
+
 	return (
 		<main>
 			<section className="accommodation-pictures">
 				{<Slideshow pictures={selectedAppartment.pictures} />}
+			</section>
+
+			<section className="accommodation-details">
+				<div className="accommodation-details__info">
+					<h2>{selectedAppartment.title}</h2>
+					<h3>{selectedAppartment.location}</h3>
+					<div className="accommodation-details__info--tags">
+						{selectedAppartment.tags.map((tag, index) => (
+							<div key={`${tag}-${index}`} className="accommodation-details__info--tag">
+								<p>{tag}</p>
+							</div>
+						))}
+					</div>
+				</div>
+
+				<div className="accommodation-details__profil">
+					<Rating rating={selectedAppartment.rating} />
+					<div className="accommodation-details__profil--host">
+						<div className="accommodation-details__profil--host-name">
+							<p>{selectedAppartment.host.name.split(" ")[0]}</p>
+							<p>{selectedAppartment.host.name.split(" ")[1]}</p>
+						</div>
+						<div className="accommodation-details__profil--host-picture">
+							<img alt={selectedAppartment.host.name} src={selectedAppartment.host.picture} />
+						</div>
+					</div>
+				</div>
 			</section>
 
 			<section className="accommodation-collapse">
